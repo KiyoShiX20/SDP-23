0da624dc2984d8af92e240cc852188cfec7389c6
Aravind <aravindsn002@gmail.com>
2023-11-16 12:29:50 +0530

    #4

--- print.py
+++ print.py
@@ -15,3 +15,25 @@ fruits.insert(0, "Orange")
 del fruits[0]
 fruits.clear()
 print(fruits)
+
+m_tuple = ["abc", 123, "IIIIII"]
+print(m_tuple)
+print(m_tuple[1:3])
+print(m_tuple[-1])
+print(m_tuple[-2])
+print(m_tuple[-3])
+
+m_set_a = {1, 2, 3, 4}
+m_set_b = {4, 5, 6}
+print(m_set_a.intersection(m_set_b))
+print(m_set_a.union(m_set_b))
+print(m_set_a.symmetric_difference(m_set_b))
+
+class Person:
+   def __init__ (self, name, age, gender):
+       self.name = name
+       self.age = age
+       self.gender = gender
+
+obj = Person("Aravind", 45, "Male")
+print(obj.name, obj.age, obj.gender)
