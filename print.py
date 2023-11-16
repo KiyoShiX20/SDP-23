9aebd40ff69754fa25693da5fc667f44d91f5160
Aravind <aravindsn002@gmail.com>
2023-11-16 10:57:28 +0530

    #1

---
+++ print.py
@@ -0,0 +1,9 @@
+def function_():
+    print(1,2,3,4, sep="#", end="&\n")
+    x = True
+    print(x, "is of type",type(x))
+function_()
+
+a = "hello"
+print(a, "is of type", type(a))
+print(a, "is of type", type(a), isinstance(a, int))
